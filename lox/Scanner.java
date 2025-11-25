6f18119b68353388d1d5bec423179fe1e1d9fbc2
siluk00 <sil-uk@hotmail.com>
2025-11-25 08:23:14 -0300

    Scanner

---
+++ lox/Scanner.java
@@ -0,0 +1,87 @@
+package lox;
+
+import static lox.TokenType.BANG_EQUAL;
+import static lox.TokenType.COMMA;
+import static lox.TokenType.DOT;
+import static lox.TokenType.EOF;
+import static lox.TokenType.GREATER_EQUAL;
+import static lox.TokenType.LEFT_BRACE;
+import static lox.TokenType.LEFT_PAREN;
+import static lox.TokenType.MINUS;
+import static lox.TokenType.PLUS;
+import static lox.TokenType.RIGHT_BRACE;
+import static lox.TokenType.RIGHT_PAREN;
+import static lox.TokenType.SEMICOLON;
+import static lox.TokenType.STAR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while(!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>': addToken(match('=') ? GREATER_EQUAL: GREATER); break;
+            default: Lox.error(line, "Unexpected character."); break;
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
+
