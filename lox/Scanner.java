58973eccf93ad3da3e12a49759de51f555cf723b
siluk00 <sil-uk@hotmail.com>
2025-11-25 10:03:01 -0300

    keywords

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,16 +1,78 @@
 package lox;
 
-import static lox.TokenType.*;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import static lox.TokenType.AND;
+import static lox.TokenType.BANG;
+import static lox.TokenType.BANG_EQUAL;
+import static lox.TokenType.CLASS;
+import static lox.TokenType.COMMA;
+import static lox.TokenType.DOT;
+import static lox.TokenType.ELSE;
+import static lox.TokenType.EOF;
+import static lox.TokenType.EQUAL;
+import static lox.TokenType.EQUAL_EQUAL;
+import static lox.TokenType.FALSE;
+import static lox.TokenType.FOR;
+import static lox.TokenType.FUN;
+import static lox.TokenType.GREATER;
+import static lox.TokenType.GREATER_EQUAL;
+import static lox.TokenType.IDENTIFIER;
+import static lox.TokenType.IF;
+import static lox.TokenType.LEFT_BRACE;
+import static lox.TokenType.LEFT_PAREN;
+import static lox.TokenType.LESS;
+import static lox.TokenType.LESS_EQUAL;
+import static lox.TokenType.MINUS;
+import static lox.TokenType.NIL;
+import static lox.TokenType.NUMBER;
+import static lox.TokenType.OR;
+import static lox.TokenType.PLUS;
+import static lox.TokenType.PRINT;
+import static lox.TokenType.RETURN;
+import static lox.TokenType.RIGHT_BRACE;
+import static lox.TokenType.RIGHT_PAREN;
+import static lox.TokenType.SEMICOLON;
+import static lox.TokenType.SLASH;
+import static lox.TokenType.STAR;
+import static lox.TokenType.STRING;
+import static lox.TokenType.SUPER;
+import static lox.TokenType.THIS;
+import static lox.TokenType.TRUE;
+import static lox.TokenType.VAR;
+import static lox.TokenType.WHILE;
 
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -48,27 +110,45 @@ public class Scanner {
             case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
             case '>': addToken(match('=') ? GREATER_EQUAL: GREATER); break;
             case '/': 
-            if (match('/')) {
-                while (peek() != '\n' && !isAtEnd()) advance();
-            }else {
-                addToken(SLASH);
-            }
-            break;
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }else {
+                    addToken(SLASH);
+                }
+                break;
             case ' ':
-                case '\r':
-                    case '\t':
-                        break;
-                        case '\n':
-                            line++;
-                            break;
+            case '\r':
+            case '\t': break;
+            case '\n':
+                line++;
+                break;
             case '"': string(); break;
             default:
-            if (isDigit(c)) {
-                number();
-            }    else {
-            Lox.error(line, "Unexpected character."); break;
-            }
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character."); break;
+                }
         }
+    }
+
+    private void identifier() {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c== '_'; 
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
